f3bc473bf628e9f24d9f8c0242c7141c56fe698c
siskaandrianit <siskaandriani1906@gmail.com>
2019-03-14 08:07:35 +0700

    Updates

---
+++ precache-manifest.51673dde9ffb37fcaf2c027c0708233e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/NextWeek/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "35b504836b0cda121802",
+    "url": "/NextWeek/static/js/runtime~main.35b50483.js"
+  },
+  {
+    "revision": "5c014996d089771bb1f9",
+    "url": "/NextWeek/static/js/main.5c014996.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/NextWeek/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "5c014996d089771bb1f9",
+    "url": "/NextWeek/static/css/main.3df51a13.chunk.css"
+  },
+  {
+    "revision": "02c5deea49ef8fe244ea0765586ce288",
+    "url": "/NextWeek/index.html"
+  }
+];
